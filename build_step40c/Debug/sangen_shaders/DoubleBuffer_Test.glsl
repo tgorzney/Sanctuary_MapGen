9f5bae578e99e9dad592e6b15a3212e064b8b9fa
Tyler Gorzney <incoming@blindminds.com>
2026-08-19 23:52:35 -0500

    Remove accidentally-committed build_step40c artifacts
    A stray parallel build directory (build_step40c/) got swept into the
    STEP24-42 commit; ignore build_*/ generally so future ad-hoc build
    dirs never get tracked again.

--- build_step40c/Debug/sangen_shaders/DoubleBuffer_Test.glsl
+++
@@ -1,8 +0,0 @@
-#version 430 core
-layout(local_size_x = WORKGROUP_SIZE) in;
-layout(std430, binding = 0) buffer Data { float values[]; };
-uniform int elementCount;
-void main() {
-    uint index = gl_GlobalInvocationID.x;
-    if (index < uint(elementCount)) values[index] *= 2.0;
-}
--- build_step40c/sangen_shaders/GpuResourceSearchPathFirst/SearchPathFirst_Test.glsl
+++
@@ -1,4 +0,0 @@
-#version 430 core
-layout(local_size_x = 1) in;
-layout(std430, binding = 0) buffer Data { float values[]; };
-void main() { values[0] = 1.0; }
